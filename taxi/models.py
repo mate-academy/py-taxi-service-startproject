47397a2fb05d42900fa8aac5030dcfd125e64ad1
Nazar-kob <kobrinnazar05@mail.com>
2022-04-02 12:04:43 +0300

    change related_name

--- taxi/models.py
+++ taxi/models.py
@@ -31,7 +31,7 @@ class Car(models.Model):
     manufacturer = models.ForeignKey(
         to=Manufacturer,
         on_delete=models.CASCADE,
-        related_name="manufacturers"
+        related_name="cars"
     )
     drivers = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name="cars")
 
