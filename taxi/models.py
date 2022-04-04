a2b8c82586024e84e1d97b889ec38b8c2c0ea625
Liuda Chuzdiuk <liuda.chuzdiuk@gmail.com>
2022-04-04 12:36:03 +0300

    'Solution'

--- taxi/models.py
+++ taxi/models.py
@@ -9,11 +9,17 @@ class Manufacturer(models.Model):
     name = models.CharField(max_length=63)
     country = models.CharField(max_length=63)
 
+    def __str__(self):
+        return self.name
+
 
 class Car(models.Model):
     model = models.CharField(max_length=63)
     manufacturer = models.ForeignKey(Manufacturer, related_name="cars", on_delete=models.CASCADE)
     drivers = models.ManyToManyField(to=settings.AUTH_USER_MODEL, related_name="cars")
+
+    def __str__(self):
+        return f"{self.model} ({self.manufacturer})"
 
 
 class Driver(AbstractUser):
@@ -27,3 +33,6 @@ class Driver(AbstractUser):
 
         verbose_name = "driver"
         verbose_name_plural = "drivers"
+
+    def __str__(self):
+        return f"{self.username} ({self.first_name} {self.last_name})"
