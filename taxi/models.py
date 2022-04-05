a837649d2b69fc508da4bcd2d3c48e3f79381e3c
Vlad Tsvigun <vl.tsv@outlook.com>
2022-04-05 23:14:30 +0300

    fix

--- taxi/models.py
+++ taxi/models.py
@@ -29,7 +29,7 @@ class Driver(AbstractUser):
 class Car(models.Model):
     model = models.CharField(max_length=255)
     manufacturer = models.ForeignKey(to=Manufacturer, on_delete=models.CASCADE)
-    drivers = models.ManyToManyField(to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    drivers = models.ManyToManyField(to=settings.AUTH_USER_MODEL)
 
     def __str__(self):
         return f"{self.manufacturer.name} {self.model} (drivers: {self.drivers})"
