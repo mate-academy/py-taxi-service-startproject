e2825d41440ec4f4c0aceaa159da0f8354df58db
Denis <sairustv228@gmail.com>
2022-06-26 23:57:33 +0300

    v1.1

--- taxi_service/settings.py
+++ taxi_service/settings.py
@@ -37,7 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'taxi',
 ]
+
+AUTH_USER_MODEL = 'taxi.Driver'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -105,7 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Kiev'
 
 USE_I18N = True
 
