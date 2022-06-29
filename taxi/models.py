3e5ab2581045d93a7d7ec3274f73eddda27230d2
Flashmobber <ufinaid@gmail.com>
2022-06-29 22:21:51 +0300

    All Done.

---
+++ taxi/models.py
@@ -0,0 +1,26 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+from taxi_service import settings
+
+
+class Manufacturer(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    country = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f"{self.name} {self.country}"
+
+
+class Driver(AbstractUser):
+    licence_number = models.CharField(max_length=255, unique=True)
+
+    class Meta:
+        verbose_name = "driver"
+        verbose_name_plural = "drivers"
+
+
+class Car(models.Model):
+    model = models.CharField(max_length=255)
+    manufacturer = models.ForeignKey(Manufacturer, on_delete=models.CASCADE)
+    drivers = models.ManyToManyField(settings.AUTH_USER_MODEL,
+                                     related_name="cars")
