18711d0fe6acd38f625376a3d0beb54a8444d795
Daria <darizhur.vol2@gmail.com>
2022-06-29 21:54:13 +0300

    'NewSolution'

--- taxi/models.py
+++ taxi/models.py
@@ -7,6 +7,9 @@ class Manufacturer(models.Model):
     name = models.CharField(max_length=255, unique=True)
     country = models.CharField(max_length=255)
 
+    def __str__(self):
+        return self.name
+
 
 class Car(models.Model):
     model = models.CharField(max_length=255)
@@ -16,6 +19,9 @@ class Car(models.Model):
         related_name="cars"
     )
 
+    def __str__(self):
+        return self.model
+
 
 class Driver(AbstractUser):
     licence_number = models.CharField(max_length=255, unique=True)
@@ -24,3 +30,6 @@ class Driver(AbstractUser):
         verbose_name = "driver"
         verbose_name_plural = "drivers"
         ordering = ["username"]
+
+    def __str__(self):
+        return self.username
