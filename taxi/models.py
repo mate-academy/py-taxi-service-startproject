8384fe98eee15a36be62b67728bdc670a59740ea
Taras Nester <tarasnester414@gmail.com>
2022-06-29 15:11:44 +0300

    added str methods to models

--- taxi/models.py
+++ taxi/models.py
@@ -8,6 +8,9 @@ class Manufacturer(models.Model):
     name = models.CharField(max_length=50, unique=True)
     country = models.CharField(max_length=50)
 
+    def __str__(self):
+        return self.name
+
 
 class Car(models.Model):
     model = models.CharField(max_length=50)
@@ -17,6 +20,9 @@ class Car(models.Model):
         related_name="cars"
     )
 
+    def __str__(self):
+        return self.model
+
 
 class Driver(AbstractUser):
     licence_number = models.CharField(max_length=50, unique=True)
@@ -24,3 +30,6 @@ class Driver(AbstractUser):
     class Meta:
         verbose_name = "driver"
         verbose_name_plural = "drivers"
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name} ({self.username})"
