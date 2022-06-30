9faa2d685f6f9a6a1ab07bdfe04bf44c6cbad053
Elena <lenasharabura@gmail.com>
2022-06-30 13:19:03 +0200

    change models.Car.manufacturer

--- taxi/models.py
+++ taxi/models.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
@@ -5,6 +6,9 @@ from django.db import models
 class Manufacturer(models.Model):
     name = models.CharField(max_length=255, unique=True)
     country = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
 
 
 class Driver(AbstractUser):
@@ -17,5 +21,8 @@ class Driver(AbstractUser):
 
 class Car(models.Model):
     model = models.CharField(max_length=255)
-    manufacturer = models.ForeignKey(Manufacturer, on_delete=models.CASCADE)
+    manufacturer = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE
+    )
     drivers = models.ManyToManyField(Driver, related_name="cars")
