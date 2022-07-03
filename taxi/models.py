a57b92bef459af67808293ec0a64c3f5129fcfe7
Serhiy Zhyhadlo <sergeyz.dev1996@gmail.com>
2022-07-03 12:25:06 +0300

    TheFix

--- taxi/models.py
+++ taxi/models.py
@@ -7,16 +7,26 @@ class Manufacturer(models.Model):
     name = models.CharField(max_length=255, unique=True)
     country = models.CharField(max_length=255)
 
+    def __str__(self):
+        return self.name
+
 
 class Car(models.Model):
     model = models.CharField(max_length=255)
     manufacturer = models.ForeignKey(Manufacturer, on_delete=models.CASCADE)
-    drivers = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    drivers = models.ForeignKey(settings.AUTH_USER_MODEL,
+                                on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.model
 
 
 class Driver(AbstractUser):
     license_number = models.CharField(max_length=255, unique=True)
 
+    def __str__(self):
+        return self.username
+
     class Meta:
-        verbose_name = 'Driver'
-        verbose_name_plural = 'Drivers'
+        verbose_name = "Driver"
+        verbose_name_plural = "Drivers"
