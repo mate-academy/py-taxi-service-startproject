d0e1dfa9859120c4509c04107836be50eeb6c62d
Viktoria Pavlej <viktorialondon16@gmail.com>
2022-08-02 22:37:19 +0100

    Solution_fixed_user_model

--- taxi/models.py
+++ taxi/models.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
-from taxi_service import settings
+from taxi_service.settings import AUTH_USER_MODEL
 
 
 class Manufacturer(models.Model):
@@ -22,7 +22,7 @@ class Driver(AbstractUser):
 class Car(models.Model):
     model = models.CharField(max_length=255)
     manufacturer = models.ForeignKey(Manufacturer, on_delete=models.CASCADE)
-    drivers = models.ManyToManyField(settings.AUTH_USER_MODEL)
+    drivers = models.ManyToManyField(AUTH_USER_MODEL)
 
     def __str__(self):
         return self.model
