178b67a889164714912f9ff7b403bf0214ea8969
Alex Parkhomenko <alex.wquels@gmail.com>
2022-10-19 16:49:55 +0300

    added type annotations

--- taxi/models.py
+++ taxi/models.py
@@ -10,7 +10,7 @@ class Driver(AbstractUser):
         verbose_name = "driver"
         verbose_name_plural = "drivers"
 
-    def __str__(self):
+    def __str__(self) -> str:
         return f"{self.first_name} {self.last_name}"
 
 
@@ -18,7 +18,7 @@ class Manufacturer(models.Model):
     name = models.CharField(max_length=255, unique=True)
     country = models.CharField(max_length=255)
 
-    def __str__(self):
+    def __str__(self) -> str:
         return self.name
 
 
@@ -31,5 +31,5 @@ class Car(models.Model):
         settings.AUTH_USER_MODEL, related_name="cars"
     )
 
-    def __str__(self):
+    def __str__(self) -> str:
         return f"{self.model} {self.manufacturer}"
