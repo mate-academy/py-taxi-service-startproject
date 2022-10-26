a1f1e5af546a9a9bfdd7f34de903d74c5545bc18
Kirill Drago <dragoswim7@gmail.com>
2022-10-26 12:41:00 +0300

    added type annotation

--- taxi/models.py
+++ taxi/models.py
@@ -7,7 +7,7 @@ class Manufacturer(models.Model):
     name = models.CharField(max_length=255, unique=True)
     country = models.CharField(max_length=63)
 
-    def __str__(self):
+    def __str__(self) -> str:
         return f"{self.name} {self.country}"
 
 
