c253676b8340e3591b6e8b713bcb4f23003370b8
Andriy Makarenko <andriy@mate.academy>
2022-10-30 15:09:59 +0200

    Solution

--- taxi/models.py
+++ taxi/models.py
@@ -11,19 +11,23 @@ class Manufacturer(models.Model):
         ordering = ["name"]
 
     def __str__(self) -> str:
-        return f"{self.name}"
+        return self.name
 
 
 class Car(models.Model):
     model = models.CharField(max_length=255)
-    manufacturer = models.ForeignKey(Manufacturer, on_delete=models.CASCADE)
-    drivers = models.ManyToManyField(settings.AUTH_USER_MODEL)
+    manufacturer = models.ForeignKey(
+        Manufacturer, on_delete=models.CASCADE, related_name="cars"
+    )
+    drivers = models.ManyToManyField(
+        settings.AUTH_USER_MODEL, related_name="cars"
+    )
 
     class Meta:
         ordering = ["model"]
 
-    def __str__(self):
-        return self.model
+    def __str__(self) -> str:
+        return f"{self.model}"
 
 
 class Driver(AbstractUser):
