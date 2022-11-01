cf2922dd8373b35aab3b7d10958f1a61005e9793
Alexander <alexzubkoff123@gmail.com>
2022-11-01 17:27:56 +0200

    Added related_name for field and fix correct naming

--- taxi/models.py
+++ taxi/models.py
@@ -32,10 +32,11 @@ class Car(models.Model):
     manufacturer = models.ForeignKey(
         Manufacturer,
         on_delete=models.CASCADE,
+        related_name="cars"
     )
     drivers = models.ManyToManyField(
         settings.AUTH_USER_MODEL,
-        related_name="drivers"
+        related_name="cars"
     )
 
     class Meta:
