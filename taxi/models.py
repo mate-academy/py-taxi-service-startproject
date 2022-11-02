0b10a654229cc370f3a2922e2bc9c2658fb7c787
Mowka7 <98344661+Mowka7@users.noreply.github.com>
2022-11-02 12:35:29 +0000

    some fixes

--- taxi/models.py
+++ taxi/models.py
@@ -8,6 +8,9 @@ class Manufacturer(models.Model):
     name = models.CharField(max_length=255, unique=True)
     country = models.CharField(max_length=255)
 
+    def __str__(self):
+        return self.name
+
 
 class Driver(AbstractUser):
     license_number = models.CharField(max_length=255, unique=True)
