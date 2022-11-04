5c4764c5fea75db1bd770fc75775848d9951d179
Oleksandr Kobzar <alexkobsar1001@gmail.com>
2022-11-04 17:33:05 -0300

    Solution

--- tests/test_taxi.py
+++ tests/test_taxi.py
@@ -7,14 +7,11 @@ import os.path
 class AdminSiteDriverTests(TestCase):
     def setUp(self) -> None:
         self.admin_user = get_user_model().objects.create_superuser(
-            username="admin.user",
-            password="1qazcde3",
+            username="admin.user", password="1qazcde3"
         )
         self.client.force_login(self.admin_user)
         self.driver = get_user_model().objects.create_user(
-            username="test.user",
-            password="2wsxvfr4",
-            license_number="TES12345"
+            username="test.user", password="2wsxvfr4", license_number="TES12345"
         )
 
     def test_license_number_in_driver_changelist(self):
@@ -40,8 +37,7 @@ class AdminSiteDriverTests(TestCase):
 class AdminSiteManufacturerTests(TestCase):
     def setUp(self) -> None:
         self.admin_user = get_user_model().objects.create_superuser(
-            username="admin.user",
-            password="1qazcde3",
+            username="admin.user", password="1qazcde3"
         )
         self.client.force_login(self.admin_user)
 
@@ -55,8 +51,7 @@ class AdminSiteManufacturerTests(TestCase):
 class AdminSiteCarTests(TestCase):
     def setUp(self) -> None:
         self.admin_user = get_user_model().objects.create_superuser(
-            username="admin.user",
-            password="1qazcde3",
+            username="admin.user", password="1qazcde3"
         )
         self.client.force_login(self.admin_user)
 
@@ -69,7 +64,7 @@ class AdminSiteCarTests(TestCase):
 
 class GitignoreTests(TestCase):
     def test_gitignore_exist(self):
-        file_exists = os.path.exists('.gitignore')
+        file_exists = os.path.exists(".gitignore")
         assert file_exists
 
     def test_gitignore_has_correct_content(self):
