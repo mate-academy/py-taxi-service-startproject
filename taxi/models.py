ef2c39765bb520f9210cec9d56249c772ea999a2
Yurii <pazyn404@gmail.com>
2022-11-09 10:12:09 +0200

    add related names, change verbose name to lower case

--- taxi/models.py
+++ taxi/models.py
@@ -14,11 +14,11 @@ class Driver(AbstractUser):
     license_number = models.CharField(max_length=255, unique=True)
 
     class Meta:
-        verbose_name = "Driver"
-        verbose_name_plural = "Drivers"
+        verbose_name = "driver"
+        verbose_name_plural = "drivers"
 
 
 class Car(models.Model):
     model = models.CharField(max_length=255)
-    manufacturer = models.ForeignKey(to=Manufacturer, on_delete=models.CASCADE)
-    drivers = models.ManyToManyField(to=Driver)
+    manufacturer = models.ForeignKey(to=Manufacturer, on_delete=models.CASCADE, related_name="cars")
+    drivers = models.ManyToManyField(to=Driver, related_name="cars")
