9df6f36334acf8496234a5aedc334e00a44183d6
IvanGLS <gladiolus0707@gmail.com>
2022-11-29 10:36:31 +0200

    'changed'

--- taxi_service/settings.py
+++ taxi_service/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-uxw+p)z#^4q+bp8b$cer6momaqnd5nqo0=uzm3yq*@t26^+q1z"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
