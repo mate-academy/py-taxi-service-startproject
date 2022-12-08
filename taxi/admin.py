85bb6f7c5731666af74cd028c6dc93a277358ac1
Bohdan2001007 <bogdan1991007@gmail.com>
2022-12-08 19:22:18 +0100

    Solution

--- taxi/admin.py
+++ taxi/admin.py
@@ -6,8 +6,7 @@ from taxi.models import Driver, Manufacturer, Car
 
 @admin.register(Car)
 class CarAdmin(admin.ModelAdmin):
-    list_display = ["model", "manufacturer"]
-    list_filter = ["model"]
+    list_filter = ["model", "manufacturer"]
     search_fields = ["model"]
 
 
@@ -16,7 +15,7 @@ class DriverAdmin(UserAdmin):
     list_display = UserAdmin.list_display + ("license_number",)
     fieldsets = UserAdmin.fieldsets + (("Additional info", {"fields": ("license_number",)}),)
     add_fieldsets = UserAdmin.add_fieldsets + (
-        ("Additional info", {"fields": ("first_name", "last_name", "license_number",)}),
+        ("Additional info", {"fields": ("license_number",)}),
     )
 
 
--- taxi_service/settings.py
+++ taxi_service/settings.py
@@ -9,8 +9,8 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
 from pathlib import Path
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'taxi_service.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates']
+        'DIRS': []
         ,
         'APP_DIRS': True,
         'OPTIONS': {
