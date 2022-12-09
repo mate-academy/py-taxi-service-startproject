82939c963297068eb7b3527a3937cce936383734
Yulia Bozhieva <bozhieva93@gmail.com>
2022-12-09 14:52:36 -0400

    'Solution'

--- taxi/models.py
+++ taxi/models.py
@@ -27,8 +27,8 @@ class Driver(AbstractUser):
     license_number = models.CharField(max_length=255, unique=True)
 
     class Meta:
-        verbose_name = "Driver"
-        verbose_name_plural = "Drivers"
+        verbose_name = "driver"
+        verbose_name_plural = "drivers"
 
     def __str__(self):
         return f"{self.username} ({self.first_name} {self.last_name})"
