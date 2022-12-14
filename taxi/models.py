efacd07eb8f7c009e7eb6b49c73c33c410fbf68d
Anton Chumak <Chumak7377@gmail.com>
2022-12-14 13:55:25 +0200

    Taxi service customizing admin panel

---
+++ taxi/models.py
@@ -0,0 +1,40 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+from taxi_service import settings
+
+
+class Manufacturer(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    country = models.CharField(max_length=255)
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self) -> str:
+        return f"{self.name}"
+
+
+class Car(models.Model):
+    model = models.CharField(max_length=255)
+    manufacturer = models.ForeignKey(
+        Manufacturer,
+        on_delete=models.CASCADE
+    )
+    drivers = models.ManyToManyField(settings.AUTH_USER_MODEL,
+                                     related_name="cars")
+
+    def __str__(self) -> str:
+        return f"{self.model} ({self.manufacturer})"
+
+
+class Driver(AbstractUser):
+    license_number = models.CharField(max_length=255, unique=True)
+
+    class Meta:
+        ordering = ["username"]
+        verbose_name = "driver"
+        verbose_name_plural = "drivers"
+
+    def __str__(self) -> str:
+        return f"{self.username} ({self.first_name} {self.last_name})"
