e0f2aad9bbc4f6a0531144ddd3781190b0f53bbf
Derkach Oleksandr <derkach03553@gmail.com>
2023-02-03 20:41:51 +0200

    Solution

--- taxi_service/settings.py
+++ taxi_service/settings.py
@@ -13,8 +13,6 @@ import os
 
 from pathlib import Path
 
-from dotenv import load_dotenv, find_dotenv
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -22,8 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-load_dotenv(find_dotenv())
-SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = "SECRET_KEY=django-insecure-j9tj^q@c+g=btzfk#)fs6d@ailkav1j&z^vmq9aim7q^^o#&b$"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
