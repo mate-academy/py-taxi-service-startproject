49e72cb88e6f918f3808159cf8fe52855e18cce2
Derkach Oleksandr <derkach03553@gmail.com>
2023-02-03 20:26:07 +0200

    Solution

--- taxi_service/settings.py
+++ taxi_service/settings.py
@@ -9,7 +9,11 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
+import os
+
 from pathlib import Path
+
+from dotenv import load_dotenv, find_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -18,8 +22,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-with open(Path(BASE_DIR, "secret.txt")) as f:
-    SECRET_KEY = f.read().strip()
+load_dotenv(find_dotenv())
+SECRET_KEY = os.environ['SECRET_KEY']
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
