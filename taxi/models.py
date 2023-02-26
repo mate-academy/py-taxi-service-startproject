979abf0a7b6803ec7ea3efbb0830e5311ae4a7bc
leetwinoff <leetwinoff92@gmail.com>
2023-02-26 13:27:00 -0600

    idea was removed

--- taxi/models.py
+++ taxi/models.py
@@ -4,15 +4,15 @@ from django.db import models
 
 
 class Manufacturer(models.Model):
-    name = models.CharField(max_length=50, unique=True)
-    country = models.CharField(max_length=50)
+    name = models.CharField(max_length=255, unique=True)
+    country = models.CharField(max_length=255)
 
     def __str__(self) -> str:
         return self.name
 
 
 class Driver(AbstractUser):
-    license_number = models.CharField(max_length=50, unique=True)
+    license_number = models.CharField(max_length=255, unique=True)
 
     class Meta:
         verbose_name = "driver"
@@ -24,7 +24,7 @@ class Driver(AbstractUser):
 
 
 class Car(models.Model):
-    model = models.CharField(max_length=50)
+    model = models.CharField(max_length=255)
     manufacturer = models.ForeignKey(Manufacturer, on_delete=models.CASCADE, related_name="cars")
     drivers = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name="cars")
 
