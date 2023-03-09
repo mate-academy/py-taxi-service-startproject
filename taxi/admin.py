447613d68b6d9d108320cc705070282b904130e3
Roman Senyshyn <senjaskorp@gmail.com>
2023-03-09 09:44:07 +0200

    Fixed add_fieldsets.

--- taxi/admin.py
+++ taxi/admin.py
@@ -7,8 +7,14 @@ from taxi.models import Manufacturer, Car, Driver
 @admin.register(Driver)
 class DriverAdmin(UserAdmin):
     list_display = UserAdmin.list_display + ("license_number",)
-    fieldsets = UserAdmin.fieldsets + (("Additional info", {"fields": ("license_number",)}),)
-    add_fieldsets = UserAdmin.add_fieldsets + (("Additional info", {"fields": ("license_number",)}),)
+    fieldsets = UserAdmin.fieldsets + (
+        ("Additional info", {"fields": ("license_number",)}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (("Additional info", {"fields": (
+        "first_name",
+        "last_name",
+        "license_number",
+    )}),)
 
 
 @admin.register(Car)
