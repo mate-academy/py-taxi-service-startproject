9a3dc5589020ce4022edb38260acb669d48d6f7c
Safonov Volodymyr <34244814+SafonovVladimir@users.noreply.github.com>
2023-03-09 08:59:10 +0200

    Solution

--- taxi/models.py
+++ taxi/models.py
@@ -17,7 +17,7 @@ class Manufacturer(models.Model):
 
 class Car(models.Model):
     model = models.CharField(max_length=63)
-    manufacturer = models.OneToOneField(
+    manufacturer = models.ForeignKey(
         Manufacturer,
         on_delete=models.CASCADE,
         related_name="cars"
