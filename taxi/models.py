d9bc68de92c639b0c7cde3c201f4a8dad51ef63e
usrofgh <mailforgimail@gmail.com>
2023-03-18 10:52:37 +0200

    solution

---
+++ taxi/models.py
@@ -0,0 +1,37 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+from taxi_service.settings import AUTH_USER_MODEL
+
+
+class Manufacturer(models.Model):
+    name = models.CharField(unique=True, max_length=100)
+    country = models.CharField(max_length=100)
+
+    def __str__(self) -> str:
+        return f"{self.name}, country - {self.country}"
+
+
+class Driver(AbstractUser):
+    license_number = models.CharField(unique=True, max_length=100)
+
+    def __str__(self) -> str:
+        return self.license_number
+
+    class Meta:
+        verbose_name = "driver"
+        verbose_name_plural = "drivers"
+
+
+class Car(models.Model):
+    model = models.CharField(max_length=100)
+    manufacturer = models.ForeignKey(
+        Manufacturer,
+        on_delete=models.CASCADE,
+        related_name="cars"
+    )
+
+    drivers = models.ManyToManyField(AUTH_USER_MODEL)
+
+    def __str__(self) -> str:
+        return f"{self.model}, {self.manufacturer}"
