ffbb674d8a20a9410ff84c68446bb60b35c36097
Bohdan Lazaryshyn <lazaryshyn1998@gmail.com>
2023-03-20 18:02:24 +0200

    Solution

--- taxi/models.py
+++ taxi/models.py
@@ -24,7 +24,7 @@ class Car(models.Model):
 
 
 class Driver(AbstractUser):
-    license_number = models.CharField(max_length=255, unique=True)
+    license_number = models.CharField(max_length=63, unique=True)
 
     def __str__(self):
         return f"{self.username} ({self.license_number})"
