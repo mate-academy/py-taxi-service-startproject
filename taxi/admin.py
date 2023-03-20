9f6b2798e12e0e89d72936c45b936a924350a5c0
Mykhailo Vovchok <mivvaw@gmail.com>
2023-03-20 21:00:29 +0200

    Sol

---
+++ taxi/admin.py
@@ -0,0 +1,25 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+
+from taxi.models import Driver, Manufacturer, Car
+
+
+@admin.register(Driver)
+class DriverAdmin(UserAdmin):
+    list_display = UserAdmin.list_display + ("license_number",)
+    fieldsets = UserAdmin.fieldsets + (("Additional info", {"fields": ("license_number",)}),)
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ("Additional info", {"fields": ("license_number", "first_name",)}),
+    )
+
+
+@admin.register(Manufacturer)
+class Manufacturer(admin.ModelAdmin):
+    list_display = ["name", "country"]
+
+
+@admin.register(Car)
+class Car(admin.ModelAdmin):
+    search_fields = ("model",)
+    list_filter = ("manufacturer",)
+    list_display = ["model", "manufacturer"]
