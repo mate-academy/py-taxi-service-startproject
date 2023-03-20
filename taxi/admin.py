65fb588100544445d3aba401b43400d5e7d55d05
VladyslavCherkashyn <Sparrowq2@gmail.com>
2023-03-20 12:21:26 +0200

    Test ignor

--- taxi/admin.py
+++ taxi/admin.py
@@ -5,7 +5,7 @@ from taxi.models import Manufacturer, Driver, Car
 
 @admin.register(Driver)
 class DriverAdmin(UserAdmin):
-    list_display = UserAdmin.list_display + ("license_number",)
+    list_display = UserAdmin.list_display + ("licenese_number",)
     fieldsets = UserAdmin.fieldsets + (
         ("Additional info", {"fields": ("license_number",)}),
     )
