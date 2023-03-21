b1a4ccd46874f343cfdf8d6538db9f52b671e174
Maksym Nikityuk <maksym71417@gmail.com>
2023-03-21 16:03:44 +0200

    Fixed

--- taxi/models.py
+++ taxi/models.py
@@ -16,8 +16,8 @@ class Manufacturer(models.Model):
 
 class Car(models.Model):
     model = models.CharField(max_length=63)
-    manufacturer = models.ForeignKey(Manufacturer, on_delete=models.CASCADE, related_name="companies")
-    drivers = models.ManyToManyField(settings.AUTH_USER_MODEL)
+    manufacturer = models.ForeignKey(Manufacturer, on_delete=models.CASCADE, related_name="cars")
+    drivers = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name="cars")
 
     def __str__(self) -> str:
         return self.model
