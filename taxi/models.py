16a92114e79dfd03fbe82049c6b5e57de9ad2b3d
Zenon <zenion@ukr.net>
2023-03-26 22:21:25 +0300

    Add `__str__()` method for each model, fix indentation and refactor Manufacturer model registration in admin panel

--- taxi/models.py
+++ taxi/models.py
@@ -8,6 +8,9 @@ class Manufacturer(models.Model):
     name = models.CharField(max_length=64, unique=True)
     country = models.CharField(max_length=64)
 
+    def __str__(self) -> str:
+        return self.name + " placed in " + self.country
+
 
 class Driver(AbstractUser):
     license_number = models.CharField(max_length=64, unique=True)
@@ -16,9 +19,19 @@ class Driver(AbstractUser):
         verbose_name = "driver"
         verbose_name_plural = "drivers"
 
+    def __str__(self) -> str:
+        return self.username + " with ID" + self.license_number
+
 
 class Car(models.Model):
     model = models.CharField(max_length=64)
-    manufacturer = models.ForeignKey(Manufacturer, on_delete=models.SET_NULL, related_name="cars", null=True)
-    drivers = models.ManyToManyField(AUTH_USER_MODEL, related_name="cars"
+    manufacturer = models.ForeignKey(
+        Manufacturer,
+        on_delete=models.SET_NULL,
+        related_name="cars",
+        null=True
     )
+    drivers = models.ManyToManyField(AUTH_USER_MODEL, related_name="cars")
+
+    def __str__(self) -> str:
+        return self.model
