5595e1b6a3d3e82ee0ab24639bb06d3ea694c88b
Vitalii Khmura <vitalikkhmura13@gmail.comw>
2023-04-05 21:54:35 +0300

    modified next files: .gitignore, taxi/admin.py

--- taxi/admin.py
+++ taxi/admin.py
@@ -9,24 +9,26 @@ from taxi.models import Driver, Car, Manufacturer
 class DriverAdmin(admin.ModelAdmin):
     list_display = UserAdmin.list_display + ("license_number",)
     fieldsets = UserAdmin.fieldsets + (
-        ("Additional info",
-         {"fields": ("license_number",)}),
+        (
+            "Additional info",
+            {"fields": ("license_number",)}
+         ),
     )
     add_fieldsets = (
         (
             "Additional info",
-            {"fields": {"license_number"}}
+            {"fields": ("license_number",)}
         ),
     )
 
 
 @admin.register(Car)
 class CarAdmin(admin.ModelAdmin):
-    list_display = ("model", )
-    search_fields = ("model", )
+    list_display = ("model",)
+    search_fields = ("model",)
     list_filter = ("manufacturer",)
 
 
 @admin.register(Manufacturer)
 class ManufacturerAdmin(admin.ModelAdmin):
-    list_display = ("name", )
+    list_display = ("name",)
