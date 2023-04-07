fa68ee2077d37c8ccc84da228ed03edec888e64e
YouRa Allakhverdov <allakhverdovyuriy@gmail.com>
2023-04-07 08:43:58 +0300

    Finish task

---
+++ taxi/admin.py
@@ -0,0 +1,29 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+
+from taxi.models import Driver, Car, Manufacturer
+
+
+@admin.register(Manufacturer)
+class ManufacturerAdmin(admin.ModelAdmin):
+    list_display = ["name", "country"]
+
+
+@admin.register(Car)
+class CarAdmin(admin.ModelAdmin):
+    search_fields = ["model"]
+    lis_filter = ["model"]
+    list_display = ["model", "manufacturer"]
+
+
+@admin.register(Driver)
+class DriverAdmin(UserAdmin):
+    list_display = UserAdmin.list_display + ("license_number",)
+    fieldsets = UserAdmin.fieldsets \
+        + (("Additional info", {"fields": ("license_number",)}),)
+    add_fieldsets = UserAdmin.add_fieldsets \
+        + (("Additional info", {"fields": (
+            "license_number",
+            "first_name",
+            "last_name"
+    )}),)
