b4ad1d23580b75ecccbc8d773cb94473ebbb4e16
Roman Harmatiuk <roman.harmatiuk@gmail.com>
2023-04-12 17:48:39 +0300

    Solution_5

--- taxi/models.py
+++ taxi/models.py
@@ -7,11 +7,11 @@ class Manufacturer(models.Model):
     name = models.CharField(max_length=63, unique=True)
     country = models.CharField(max_length=63)
 
-    def __str__(self):
-        return self.name
-
     class Meta:
         ordering = ["name"]
+
+    def __str__(self):
+        return self.name
 
 
 class Driver(AbstractUser):
@@ -30,8 +30,8 @@ class Car(models.Model):
     )
     drivers = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name="car")
 
-    def __str__(self):
-        return self.model
-
     class Meta:
         ordering = ["model"]
+
+    def __str__(self):
+        return self.model
