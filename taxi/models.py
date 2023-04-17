103188d4deb8419a9710f0681afddb81bef532d5
Artem Kurmanchuk <bosskurmanchuk143@gmail.com>
2023-04-17 15:25:24 +0300

    Solution 3

--- taxi/models.py
+++ taxi/models.py
@@ -15,8 +15,8 @@ class Driver(AbstractUser):
     license_number = models.CharField(max_length=255, unique=True)
 
     class Meta:
-        verbose_name = "Driver"
-        verbose_name_plural = "Drivers"
+        verbose_name = "driver"
+        verbose_name_plural = "drivers"
 
 
 class Car(models.Model):
