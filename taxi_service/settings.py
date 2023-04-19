e8988c2313ed104fc61a7224132b125da2f4c5f0
Mkrtych Khachatrian <mukuchbrat@gmail.com>
2023-04-19 13:11:11 +0300

    removed dotenv for now and fixed all

--- taxi_service/settings.py
+++ taxi_service/settings.py
@@ -9,8 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-import os
-from dotenv import load_dotenv
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -21,8 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-load_dotenv()  # loads the configs from .env
-SECRET_KEY = str(os.getenv('SECRET_KEY'))
+SECRET_KEY = "django-insecure-6rnc@wp$8v0ua94s8%kuo74h5l97^ntsxc_2#9uxmwcq--$*z-"
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
