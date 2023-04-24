c9c827bc54d6685dcac1d67c2be6b26911359b6a
kostya-kononenko <kkononenko3@gmail.com>
2023-04-24 16:47:27 +0300

    Solution1

--- taxi/models.py
+++ taxi/models.py
@@ -16,15 +16,15 @@ class Driver(AbstractUser):
     license_number = models.CharField(max_length=255, unique=True)
 
     class Meta:
-        verbose_name_plural = "Drivers"
+        verbose_name = "driver"
+        verbose_name_plural = "drivers"
 
 
 class Car(models.Model):
     model = models.CharField(max_length=255)
     manufacturers = models.ForeignKey(Manufacturer,
                                       on_delete=models.CASCADE)
-    drivers = models.ManyToManyField(settings.AUTH_USER_MODEL,
-                                     verbose_name="Driver")
+    drivers = models.ManyToManyField(settings.AUTH_USER_MODEL)
 
     def __str__(self):
         return self.model
