275e32ce712d212af6d22e10d5a3c9fc629b40bd
Vladyslav <vladyslavkhokhlov@gmail.com>
2023-06-12 14:34:14 +0300

    Solution

--- taxi/admin.py
+++ taxi/admin.py
@@ -5,16 +5,19 @@ from taxi.models import Manufacturer, Driver, Car
 
 
 @admin.register(Manufacturer)
-class ManufacturerAdmin:
+class ManufacturerAdmin(admin.ModelAdmin):
     list_display = ["name", "country"]
 
 
 @admin.register(Car)
-class CarAdmin:
-    list_display = ["model", "manufacturer", "drivers"]
+class CarAdmin(admin.ModelAdmin):
+    list_display = ["model", "manufacturer", "get_drivers"]
     list_filter = ["manufacturer"]
     search_fields = ["model"]
 
+    @staticmethod
+    def get_drivers(obj):
+        return "\n".join([p.drivers for p in obj.drivers.all()])
 
 @admin.register(Driver)
 class DriverAdmin(UserAdmin):
--- taxi/models.py
+++ taxi/models.py
@@ -17,7 +17,7 @@ class Manufacturer(models.Model):
     country = models.CharField(max_length=255)
 
     class Meta:
-        ordering = "name"
+        ordering = ["name"]
 
     def __str__(self):
         return f"{self.name}({self.country})"
