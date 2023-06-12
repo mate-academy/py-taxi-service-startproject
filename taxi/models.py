cdde2feb2b06de06021feffb931843c0adfa0752
Ivan <ivan.korshunov.1997@gmail.com>
2023-06-12 15:57:16 +0300

    remove capitals from verbose names

--- taxi/models.py
+++ taxi/models.py
@@ -30,5 +30,5 @@ class Driver(AbstractUser):
     license_number = models.CharField(max_length=255, unique=True)
 
     class Meta:
-        verbose_name = "Driver"
-        verbose_name_plural = "Drivers"
+        verbose_name = "driver"
+        verbose_name_plural = "drivers"
