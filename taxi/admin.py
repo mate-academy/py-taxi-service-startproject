9cea2bc3fde0d060ad91b9f32c3b2915cec10df0
Anatoliy Pilipey <pilipeytolya@gmail.com>
2023-06-22 18:01:02 +0300

    Solution

--- taxi/admin.py
+++ taxi/admin.py
@@ -6,10 +6,12 @@ from taxi.models import Driver, Car, Manufacturer
 @admin.register(Driver)
 class DriverAdmin(UserAdmin):
     list_display = UserAdmin.list_display + ("license_number",)
-    fieldsets = UserAdmin.fieldsets +\
-        (("Additional info", {"fields": ("license_number",)}),)
-    add_fieldsets = UserAdmin.add_fieldsets +\
-        (("Additional info", {"fields": ("license_number",)}),)
+    fieldsets = UserAdmin.fieldsets + (
+        ("Additional info", {"fields": ("license_number",)}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ("Additional info", {"fields": ("license_number",)}),
+    )
 
 
 @admin.register(Car)
