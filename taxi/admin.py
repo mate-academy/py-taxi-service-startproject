820cbf8b42441336edca300bd54e13cff2abc169
Denys Seliverstov <catsperr322@gmail.com>
2023-07-11 15:57:41 +0300

    Applying some changes

--- taxi/admin.py
+++ taxi/admin.py
@@ -11,22 +11,16 @@ class ManufacturerAdmin(admin.ModelAdmin):
 
 @admin.register(Car)
 class CarAdmin(admin.ModelAdmin):
-    search_fields = ("model", )
-    list_filter = ("manufacturer", )
+    search_fields = ("model",)
+    list_filter = ("manufacturer",)
 
 
 @admin.register(Driver)
 class DriverAdmin(UserAdmin):
-    list_display = UserAdmin.list_display + ("license_number", )
+    list_display = UserAdmin.list_display + ("license_number",)
     fieldsets = UserAdmin.fieldsets + (
-        (
-            "Additional info", {"fields": ("license_number",)}
-        ),
+        ("Additional info", {"fields": ("license_number",)}),
     )
     add_fieldsets = UserAdmin.add_fieldsets + (
-        (
-            "Additional info", {"fields": ("license_number", )}
-        ),
+        ("Additional info", {"fields": ("license_number",)}),
     )
-
-
