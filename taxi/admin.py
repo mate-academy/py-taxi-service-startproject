29cb9f7278f1bcd2904008cb59f7d46d4f0916b9
Vlad <vladislav.zhyla@gmail.com>
2023-07-17 16:36:22 +0300

    Solution v3

--- taxi/admin.py
+++ taxi/admin.py
@@ -8,16 +8,10 @@ from taxi.models import Car, Manufacturer, Driver
 class DriverAdmin(UserAdmin):
     list_display = UserAdmin.list_display + ("license_number",)
     fieldsets = UserAdmin.fieldsets + (
-        (
-            "Additional info",
-            {"fields": ("license_number",)}
-        ),
+        ("Additional info", {"fields": ("license_number",)}),
     )
     add_fieldsets = UserAdmin.add_fieldsets + (
-        (
-            "Additional info",
-            {"fields": ("license_number",)}
-        ),
+        ("Additional info", {"fields": ("license_number",)}),
     )
 
 
--- taxi/models.py
+++ taxi/models.py
@@ -6,7 +6,6 @@ class Driver(AbstractUser):
     license_number = models.CharField(max_length=255, unique=True)
 
     class Meta:
-        verbose_name = "driver"
         verbose_name_plural = "drivers"
 
 
@@ -20,6 +19,7 @@ class Manufacturer(models.Model):
 
 class Car(models.Model):
     model = models.CharField(max_length=255)
-    manufacturer = models.ForeignKey(to=Manufacturer, related_name="Cars", on_delete=models.DO_NOTHING)
-    drivers = models.ManyToManyField(to=Driver, related_name="Cars")
-
+    manufacturer = models.ForeignKey(
+        to=Manufacturer, related_name="cars", on_delete=models.DO_NOTHING
+    )
+    drivers = models.ManyToManyField(to=Driver, related_name="cars")
