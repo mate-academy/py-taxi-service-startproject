a44dcec6a3217c0dd717358c478a1967962b5aa1
Oleksiy Lyashenko <oleksij.lyashenko@gmail.com>
2023-07-26 16:40:50 +0300

    -created db and models

---
+++ taxi/models.py
@@ -0,0 +1,44 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.conf import settings
+
+
+class Manufacturer(models.Model):
+    name = models.CharField(max_length=63, unique=True)
+    country = models.CharField(max_length=63)
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self):
+        return f"{self.name} created in {self.country}"
+
+
+class Driver(AbstractUser):
+    license_number = models.CharField(
+        max_length=63,
+        unique=True
+    )
+
+    class Meta:
+        verbose_name = "driver"
+        verbose_name_plural = "drivers"
+
+
+class Car(models.Model):
+    model = models.CharField(max_length=63)
+    manufacturer = models.ForeignKey(
+        Manufacturer,
+        on_delete=models.CASCADE,
+        related_name="car"
+    )
+    drivers = models.ManyToManyField(
+        settings.AUTH_USER_MODEL,
+        related_name="cars"
+    )
+
+    class Meta:
+        ordering = ["model"]
+
+    def __str__(self):
+        return self.model
