158291b2bd316eb19708d86250dd3a68df6a76bb
Oleksandr Ilchenko <oleksandrilchenko@Air-Oleksandr.fritz.box>
2023-08-09 21:43:17 +0200

    Update

--- taxi/models.py
+++ taxi/models.py
@@ -4,15 +4,15 @@ from django.conf import settings
 
 
 class Manufacturer(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, unique=True)
     country = models.CharField(max_length=100)
 
-    def __str__(self):
+    def __str__(self) -> str:
         return self.name
 
 
 class Driver(AbstractUser):
-    license_number = models.CharField(max_length=100)
+    license_number = models.CharField(max_length=100, unique=True)
 
     class Meta:
         verbose_name = "driver"
@@ -21,5 +21,5 @@ class Driver(AbstractUser):
 
 class Car(models.Model):
     model = models.CharField(max_length=100)
-    manufacturer = models.ForeignKey(Manufacturer, on_delete=models.CASCADE)
-    drivers = models.ManyToManyField(settings.AUTH_USER_MODEL)
+    manufacturer = models.ForeignKey(Manufacturer, on_delete=models.CASCADE, related_name="manufacturer")
+    drivers = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name="drivers")
