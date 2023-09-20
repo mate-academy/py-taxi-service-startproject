c1fd9f956de05e7cd76946ae624b629ab17e5cae
Olexander Bryl <edlrian814@gmail.com>
2023-09-20 18:32:45 +0300

    corrected3

--- taxi_service/settings.py
+++ taxi_service/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-import decouple
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = decouple.config("SECRET_KEY")
+SECRET_KEY = "SECRET"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
