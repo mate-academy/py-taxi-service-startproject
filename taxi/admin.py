b7e28f7b8991abbbfe97acf25f1c3a46a1bf04f8
Nazarii Schcur <nazae530@gmail.com>
2023-10-08 21:29:05 +0300

    solution

--- taxi/admin.py
+++ taxi/admin.py
@@ -1,7 +1,27 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-from taxi.models import Car, Manufacturer, Driver
+from taxi.models import Manufacturer, Driver, Car
 
-admin.site.register(Car)
-admin.site.register(Manufacturer)
-admin.site.register(Driver)
+
+@admin.register(Driver)
+class DriverAdmin(admin.ModelAdmin):
+    list_display = UserAdmin.list_display + ("license_number",)
+    fieldsets = UserAdmin.fieldsets + (
+        ("Additional info", {"fields": ("license_number",)}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ("Additional info", {"fields": ("license_number",)}),
+    )
+
+
+@admin.register(Car)
+class CarAdmin(admin.ModelAdmin):
+    list_display = ("model", "manufacturer")
+    search_fields = ("model",)
+    list_filter = ("manufacturer__name",)
+
+
+@admin.register(Manufacturer)
+class ManufacturerAdmin(admin.ModelAdmin):
+    pass
--- taxi/models.py
+++ taxi/models.py
@@ -1,24 +1,28 @@
-
-from django.contrib.auth.models import AbstractUser
 from django.db import models
-
-
-from taxi_service import settings
+from django.contrib.auth.models import AbstractUser
+from django.conf import settings
 
 
 class Manufacturer(models.Model):
-    name = models.CharField(max_length=63)
-    coutry = models.CharField(max_length=63)
+    name = models.CharField(max_length=100, unique=True)
+    country = models.CharField(max_length=100, default="country")
+
+    def __str__(self) -> str:
+        return self.name
 
 
 class Driver(AbstractUser):
-    license_number = models.CharField(max_length=255)
+    license_number = models.CharField(max_length=100, unique=True)
+
+    class Meta:
+        verbose_name = "driver"
+        verbose_name_plural = "drivers"
 
 
 class Car(models.Model):
-    model = models.CharField(max_length=255)
-    manufacturer = models.ForeignKey(Manufacturer, on_delete=models.CASCADE)
-    drivers = models.ManyToManyField(
-        Driver,
-        related_name="cars"
-    )
+    model = models.CharField(max_length=100)
+    manufacturer = models.ForeignKey(Manufacturer, on_delete=models.CASCADE, related_name="cars")
+    drivers = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name="cars")
+
+    def __str__(self) -> str:
+        return f"{self.manufacturer.name} {self.model}"
