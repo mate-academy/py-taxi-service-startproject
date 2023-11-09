398e9bf51093bc6858ec17feeba764fcd75cee39
Nadiia <nadiia.developer777@gmail.com>
2023-11-09 12:12:13 +0200

    Solution-four-py-taxi-service-startproject

---
+++ taxi/models.py
@@ -0,0 +1,34 @@
+from django.conf import settings
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class Manufacturer(models.Model):
+    name = models.CharField(max_length=30, unique=True)
+    country = models.CharField(max_length=30)
+
+    def __str__(self):
+        return f"{self.name} {self.country}"
+
+
+class Driver(AbstractUser):
+    license_number = models.CharField(max_length=30, null=True, blank=True, unique=True)
+    username = models.CharField(max_length=30, unique=True)
+
+    class Meta:
+        ordering = ["license_number"]
+
+    def __str__(self):
+        return f"{self.username} {self.license_number} {self.first_name} {self.last_name}"
+
+
+class Car(models.Model):
+    model = models.CharField(max_length=30)
+    manufacturer = models.ForeignKey(Manufacturer, on_delete=models.CASCADE, related_name="manufacturer")
+    drivers = models.ManyToManyField(settings.AUTH_USER_MODEL)
+
+    class Meta:
+        ordering = ["manufacturer"]
+
+    def __str__(self):
+        return f"{self.model} {self.manufacturer} {self.drivers}"
