e3a7a8ecdd721a034de947870c69a549181b8dc6
Denys <den.prislipskyi@gmail.com>
2023-11-13 17:06:53 +0100

    second

---
+++ taxi/models.py
@@ -0,0 +1,30 @@
+from django.conf import settings
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class Manufacturer(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    country = models.CharField(max_length=255)
+
+    def __str__(self) -> str:
+        return f"name: {self.name}, country{self.country}"
+
+
+class Car(models.Model):
+    model = models.CharField(max_length=255)
+    manufacturer = models.ForeignKey(Manufacturer,
+                                     on_delete=models.CASCADE,
+                                     related_name="cars")
+    drivers = models.ManyToManyField(settings.AUTH_USER_MODEL,
+                                     related_name="cars")
+
+    def __str__(self) -> str:
+        return self.model
+
+
+class Driver(AbstractUser):
+    license_number = models.CharField(max_length=255, unique=True)
+
+    class Meta:
+        ordering = ("username",)
