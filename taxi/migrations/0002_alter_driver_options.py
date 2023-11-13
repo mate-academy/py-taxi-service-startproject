c03545113ccf4911b8fcc8f2c77dd6632d64163f
Daniil Pankieiev <daniil.pankieiev@gmail.com>
2023-11-13 15:54:11 +0200

    Solution

--- taxi/migrations/0002_alter_driver_options.py
+++ taxi/migrations/0002_alter_driver_options.py
@@ -4,14 +4,13 @@ from django.db import migrations
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
-        ('taxi', '0001_initial'),
+        ("taxi", "0001_initial"),
     ]
 
     operations = [
         migrations.AlterModelOptions(
-            name='driver',
-            options={'verbose_name': 'Driver', 'verbose_name_plural': 'Drivers'},
+            name="driver",
+            options={"verbose_name": "Driver", "verbose_name_plural": "Drivers"},
         ),
     ]
--- taxi/models.py
+++ taxi/models.py
@@ -5,15 +5,15 @@ from taxi_service.settings import AUTH_USER_MODEL
 
 
 class Manufacturer(models.Model):
-    name = models.CharField(max_length=255, unique=True)
-    country = models.CharField(max_length=255)
+    name = models.CharField(max_length=65, unique=True)
+    country = models.CharField(max_length=65)
 
     def __str__(self):
         return self.name
 
 
 class Car(models.Model):
-    model = models.CharField(max_length=255)
+    model = models.CharField(max_length=65)
     manufacturer = models.ForeignKey(Manufacturer, on_delete=models.CASCADE)
     drivers = models.ManyToManyField(AUTH_USER_MODEL)
 
@@ -22,11 +22,11 @@ class Car(models.Model):
 
 
 class Driver(AbstractUser):
-    license_number = models.CharField(max_length=255, unique=True)
+    license_number = models.CharField(max_length=75, unique=True)
 
     class Meta:
-        verbose_name = 'Driver'
-        verbose_name_plural = 'Drivers'
+        verbose_name = "Driver"
+        verbose_name_plural = "Drivers"
 
     def __str__(self):
         return f"{self.username} {self.license_number}"
