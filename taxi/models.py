634f4eca39d699188d72af37d158178af641877a
sakhaline <alexandra.ivva@gmail.com>
2023-11-14 15:37:05 +0200

    Add related names

--- taxi/models.py
+++ taxi/models.py
@@ -14,7 +14,7 @@ class Manufacturer(models.Model):
 class Car(models.Model):
     model = models.CharField(max_length=63)
     manufacturer = models.ForeignKey(Manufacturer, on_delete=models.CASCADE)
-    drivers = models.ManyToManyField(AUTH_USER_MODEL)
+    drivers = models.ManyToManyField(AUTH_USER_MODEL, related_name="cars")
 
     def __str__(self) -> str:
         return self.model
