e507ee4dd8b411edc238e0281795671490dc4cf7
asdadaversa <peikqudakeus@gmail.com>
2023-11-14 18:07:16 +0200

    Solution

--- tests/test_taxi.py
+++ tests/test_taxi.py
@@ -71,7 +71,7 @@ class AdminSiteCarTests(TestCase):
 
 class GitignoreTests(TestCase):
     def test_gitignore_exist(self):
-        file_exists = os.path.exists("../.gitignore")
+        file_exists = os.path.exists(".gitignore")
         assert file_exists
 
     def test_gitignore_has_correct_content(self):
