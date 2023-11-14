ec4549f58f949b6727799075d051853bf69243e9
mykolamateichuk <mateichukmykola@gmail.com>
2023-11-14 21:25:54 +0200

    Solution_v2

--- taxi/models.py
+++ taxi/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.conf import settings
 
 
 class Manufacturer(models.Model):
@@ -12,14 +13,9 @@ class Manufacturer(models.Model):
 
 class Driver(AbstractUser):
     license_number = models.CharField(max_length=255, unique=True)
-    username = models.CharField(max_length=255, unique=True)
-    email = models.CharField(max_length=255)
-    password = models.CharField(max_length=255)
-    first_name = models.CharField(max_length=255)
-    last_name = models.CharField(max_length=255)
 
 
 class Car(models.Model):
     model = models.CharField(max_length=255)
     manufacturer = models.ForeignKey(Manufacturer, on_delete=models.CASCADE)
-    drivers = models.ManyToManyField(Driver, related_name="drivers")
+    drivers = models.ManyToManyField(settings.AUTH_USER_MODEL)
