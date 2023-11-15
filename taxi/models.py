cde37576b7c65fb19e2460a4989bb279d7383124
Rostyslav Chornyi <145897105+RVChornyy@users.noreply.github.com>
2023-11-15 18:31:22 +0200

    Update models.py

--- taxi/models.py
+++ taxi/models.py
@@ -14,7 +14,7 @@ class Manufacturer(models.Model):
 class Car(models.Model):
     model = models.CharField(max_length=63)
     manufacturer = models.ForeignKey(Manufacturer, on_delete=models.CASCADE, related_name="cars")
-    drivers: models.ManyToManyField(settings.AUTH_USER_MODEL)
+    drivers = models.ManyToManyField(settings.AUTH_USER_MODEL)
 
     def __str__(self):
         return f"{self.manufacturer} {self.model}"
