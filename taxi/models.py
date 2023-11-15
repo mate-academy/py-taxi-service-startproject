a41fe8b9a367078e2aa8bbd7e037f6115f1b2e92
asdadaversa <peikqudakeus@gmail.com>
2023-11-15 16:28:10 +0200

    Solution

--- taxi/models.py
+++ taxi/models.py
@@ -15,10 +15,10 @@ class Manufacturer(models.Model):
 class Car(models.Model):
     model = models.CharField(max_length=255)
     manufacturer = models.ForeignKey(
-        Manufacturer, on_delete=models.CASCADE, related_name="manufacturer"
+        Manufacturer, on_delete=models.CASCADE, related_name="manufacturers"
     )
 
-    drivers = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name="driver")
+    drivers = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name="cars")
 
     def __str__(self) -> str:
         return f"model: {self.model}, manufacturer: {self.manufacturer}"
