dbf228a1aad1c0d38dd84cda6168ec3d2017ee08
Roman Zhadan <21gehassig@gmail.com>
2023-11-16 14:20:12 +0200

    'Solution'

--- taxi/models.py
+++ taxi/models.py
@@ -28,5 +28,5 @@ class Driver(AbstractUser):
     class Meta:
         ordering = ["username"]
 
-    def __str__(self):
+    def __str__(self) -> str:
         return f"{self.username} ({self.first_name} {self.last_name})"
