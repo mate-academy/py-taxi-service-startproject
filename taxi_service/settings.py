fefb8f978c72c4941a8d24aa2db432e60eefe7d1
Dzhura Rostislav <dzhurarostislav@gmail.com>
2023-11-21 22:21:12 +0200

    add secret key

--- taxi_service/settings.py
+++ taxi_service/settings.py
@@ -19,6 +19,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = '12'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
