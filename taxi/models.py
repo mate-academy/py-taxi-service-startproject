749ec192d933bae57b56ae5656839c18892fc7d9
Olexandr Hontarenko <sanya221138@gmail.com>
2023-11-22 19:40:05 +0200

    fixed Solution

--- taxi/models.py
+++ taxi/models.py
@@ -7,12 +7,21 @@ class Manufacturer(models.Model):
     name = models.CharField(max_length=255, unique=True)
     country = models.CharField(max_length=255)
 
+    def __str__(self) -> str:
+        return self.name
+
 
 class Driver(AbstractUser):
     license_number = models.CharField(max_length=255, unique=True)
+
+    def __str__(self) -> str:
+        return f"{self.first_name} {self.last_name}"
 
 
 class Car(models.Model):
     model = models.CharField(max_length=255)
     manufacturer = models.ForeignKey(Manufacturer, on_delete=models.CASCADE)
     drivers = models.ManyToManyField(settings.AUTH_USER_MODEL)
+
+    def __str__(self) -> str:
+        return f"{self.model} {self.manufacturer.name}"
