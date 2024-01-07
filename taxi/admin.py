946573c6d3a59b4c65419138383fa7ac71d8d5c5
Vladyslav Shepilov <shepsterya@gmail.com>
2024-01-07 23:11:53 +0200

    resloved

--- taxi/admin.py
+++ taxi/admin.py
@@ -7,17 +7,24 @@ from taxi.models import Driver, Car, Manufacturer
 @admin.register(Driver)
 class DriverAdmin(UserAdmin):
     list_display = UserAdmin.list_display + ("license_number",)
-    fieldsets = UserAdmin.fieldsets + (("Additional info", {"fields": ("license_number", )}), )
+    fieldsets = UserAdmin.fieldsets + (("info", {"fields": ("license_number",)}),)
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (
+            "Additional info",
+            {
+                "fields": ("license_number",),
+            },
+        ),
+    )
 
 
 @admin.register(Car)
 class CarAdmin(admin.ModelAdmin):
-    list_display = ["model", "manufacturer", ]
-    list_filter = ["model", ]
-    search_fields = ["manufacturer", ]
+    list_display = ["model", "manufacturer"]
+    list_filter = ["model"]
+    search_fields = ["manufacturer"]
 
 
 @admin.register(Manufacturer)
 class ManufacturerAdmin(admin.ModelAdmin):
-    list_display = ["name", "country", ]
-
+    list_display = ["name", "country"]
