614cc322df2d153651d8f6b235d3372e32bc6ae7
Artem <temakot254@gmail.com>
2024-01-13 13:00:13 +0200

    Solution

--- taxi/admin.py
+++ taxi/admin.py
@@ -5,21 +5,16 @@ from django.contrib.auth.admin import UserAdmin
 
 class DriverAdmin(UserAdmin):
     model = Driver
-    list_display = ['username', 'email', 'license_number']
-
-    fieldsets = (
-        (None, {'fields': ('username', 'password')}),
-        ('Personal info', {'fields': ('email', 'first_name', 'last_name')}),
-        ('Additional info', {'fields': ('license_number',)}),  # Include license_number in 'Additional info'
-        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
-        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+    list_display = UserAdmin.list_display + (
+        "license_number",
     )
 
-    add_fieldsets = (
-        (None, {
-            'classes': ('wide',),
-            'fields': ('username', 'email', 'password1', 'password2', 'license_number')},
-         ),
+    fieldsets = UserAdmin.fieldsets + (
+        ("Additional info", {"fields":("license_number",)}),
+    )
+
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ("Additional info", {"fields":("license_number",)}),
     )
 
 
