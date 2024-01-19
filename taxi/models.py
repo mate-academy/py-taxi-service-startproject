1580763520d16bc26c043512f7d90b55872ed32f
Maxim V <m4k51m15k4m@gmail.com>
2024-01-19 22:28:45 +0200

    Solution

--- taxi/models.py
+++ taxi/models.py
@@ -1,3 +1,35 @@
+from django.contrib.auth.models import AbstractUser
+
 from django.db import models
 
-# Create your models here.
+
+class Manufacturer(models.Model):
+    name = models.CharField(max_length=63)
+    country = models.CharField(max_length=63)
+
+    def __str__(self):
+        return f"{self.name} ({self.country})"
+
+
+class Driver(AbstractUser, ):
+    license_number = models.CharField(max_length=63, unique=True, )
+    email = models.EmailField()
+    password = models.CharField(max_length=63, )
+    first_name = models.CharField(max_length=63, )
+    last_name = models.CharField(max_length=63, )
+
+
+def __str__(self, ):
+    return f"{self.first_name} {self.last_name}"
+
+
+class Car(models.Model, ):
+    model = models.CharField(max_length=63, )
+    manufacturer = models.OneToOneField(Manufacturer,
+                                        on_delete=models.CASCADE,
+                                        unique=True,
+                                        )
+    drivers = models.ManyToManyField(Driver, blank=True, )
+
+    def __str__(self, ):
+        return f"{self.model}"
--- taxi_service/settings.py
+++ taxi_service/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
@@ -27,7 +26,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -37,6 +35,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "taxi_service",
+    "taxi"
 ]
 
 MIDDLEWARE = [
@@ -69,7 +69,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "taxi_service.wsgi.application"
 
-
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
@@ -79,7 +78,6 @@ DATABASES = {
         "NAME": BASE_DIR / "db.sqlite3",
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
@@ -99,7 +97,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
@@ -111,7 +108,6 @@ USE_I18N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
@@ -121,3 +117,5 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+AUTH_USER_MODEL = "taxi.Driver"
