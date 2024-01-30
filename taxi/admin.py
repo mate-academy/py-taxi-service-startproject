78df67ce8dcb82e1dfaf2d967c5d2912e609d762
Kate Chernyavska <ccarts.kpi@gmail.com>
2024-01-30 12:07:20 +0200

    created models and edited file admin

---
+++ taxi/admin.py
@@ -0,0 +1,38 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+
+from taxi.models import Car, Driver, Manufacturer
+
+
+@admin.register(Car)
+class CarAdmin(admin.ModelAdmin):
+    search_fields = ["model"]
+    list_filter = ["manufacturer"]
+
+
+@admin.register(Driver)
+class DriverAdmin(UserAdmin):
+    list_display = ["license_number"]
+    fieldsets = UserAdmin.fieldsets + (
+        (("Additional info", {"fields": ("license_number",)}),)
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (
+            (
+                "Additional info",
+                {
+                    "fields": (
+                        "first_name",
+                        "last_name",
+                        "license_number",
+                    )
+                },
+            ),
+        )
+    )
+
+
+admin.site.register(Manufacturer)
+
+
+# Register your models here.
---
+++ taxi/models.py
@@ -0,0 +1,43 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+from taxi_service import settings
+
+
+# Create your models here.
+class Manufacturer(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    country = models.CharField(max_length=255)
+
+    class Meta:
+        verbose_name = "manufacturer"
+        verbose_name_plural = "manufacturers"
+
+    def __str__(self):
+        return self.name
+
+
+class Driver(AbstractUser):
+    license_number = models.CharField(max_length=255, unique=True)
+
+    class Meta:
+        verbose_name = "driver"
+        verbose_name_plural = "drivers"
+
+    def __str__(self):
+        return self.username
+
+
+class Car(models.Model):
+    model = models.CharField(max_length=255)
+    manufacturer = models.ForeignKey(Manufacturer, on_delete=models.CASCADE)
+    driver = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name="cars")
+
+    class Meta:
+        verbose_name = "car"
+        verbose_name_plural = "cars"
+
+    def __str__(self):
+        return self.model
+
+
