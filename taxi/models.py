5bdba4e26f58553328bc92f15444329d727309ba
Mykola Setrakov <nikset1@gmail.com>
2024-02-01 23:43:24 +0200

    Solution

---
+++ taxi/models.py
@@ -0,0 +1,36 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+from django.db.models import UniqueConstraint
+
+
+class Manufacturer(models.Model):
+    name = models.CharField(max_length=63)
+    country = models.CharField(max_length=63)
+
+    class Meta:
+        constraints = [
+            UniqueConstraint(fields=["name"], name="unique_name")
+        ]
+
+
+class Car(models.Model):
+    model = models.CharField(max_length=63)
+    manufacturer = models.ForeignKey(
+        Manufacturer,
+        on_delete=models.CASCADE,
+        related_name="cars"
+    )
+    drivers = models.ManyToManyField(
+        "Driver",
+        related_name="cars"
+    )
+
+
+class Driver(AbstractUser):
+    license_number = models.CharField(max_length=63)
+
+    class Meta:
+        constraints = [
+            UniqueConstraint(fields=["license_number"],
+                             name="unique_license_number")
+        ]
