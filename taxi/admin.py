51f7d89100e854e2fb8a6c40b675e675958f121c
Misha <mishelgrotski9@gmail.com>
2024-02-03 21:50:46 -0700

    gitignore update

--- taxi/admin.py
+++ taxi/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from taxi.models import Car, Driver, Manufactured
+
+
+@admin.register(Car)
+class CarAdmin(admin.ModelAdmin):
+    list_display = ["model", "manufactured", "drivers_list", ]
+    search_fields = ["model", ]
+    list_filter = ["manufactured", ]
+
+    def drivers_list(self, drive):
+        return ", ".join([driver.username for driver in drive.drivers.all()])
+
+
+@admin.register(Driver)
+class DriverAdmin(UserAdmin):
+    list_display = UserAdmin.list_display + ("license_number", )
+    fieldsets = UserAdmin.fieldsets + (("Additional info", {"fields": ("license_number",)}),)
+    add_fieldset = UserAdmin.add_fieldsets + (("Additional info", {"fields": ("license_number",)}),)
+
+
+@admin.register(Manufactured)
+class ManufacturedAdmin(admin.ModelAdmin):
+    list_display = ["name", "country", ]
--- taxi/models.py
+++ taxi/models.py
@@ -1,3 +1,36 @@
+from django.conf import settings
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+
+class Manufactured(models.Model):
+    name = models.CharField(max_length=63)
+    country = models.CharField(max_length=63)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=["name"],
+                name="unique_name"
+            )
+        ]
+
+
+class Car(models.Model):
+    model = models.CharField(max_length=63)
+    manufactured = models.ForeignKey("Manufactured", on_delete=models.CASCADE)
+    drivers = models.ManyToManyField("Driver", related_name="cars")
+
+
+class Driver(AbstractUser):
+    license_number = models.CharField(max_length=100, blank=True, null=True)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=["license_number"],
+                name="unique_license_number"
+            )
+        ]
+
