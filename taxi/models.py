e0e5fcaa7c5eba68c9814404c49889d70a65e525
Maksym Symonovych <maksym.symonovych@gmail.com>
2024-02-22 13:15:06 +0200

    fix models.py

--- taxi/models.py
+++ taxi/models.py
@@ -5,32 +5,24 @@ from django.db import models
 
 class Manufacturer(models.Model):
     name = models.CharField(max_length=50, unique=True)
-    country = models.CharField(max_length=15)
+    country = models.CharField(max_length=50)
 
-    class Meta:
-        verbose_name = "manufacturer"
-        verbose_name_plural = "manufacturers"
-
-    def __str__(self):
+    def __str__(self) -> str:
         return self.name
 
 
 class Car(models.Model):
     model = models.CharField(max_length=25)
-    manufacturer = models.ForeignKey(Manufacturer, on_delete=models.CASCADE)
+    manufacturer = models.ForeignKey(
+        Manufacturer,
+        on_delete=models.CASCADE,
+        related_name='cars'
+    )
     drivers = models.ManyToManyField(
         settings.AUTH_USER_MODEL,
         related_name="cars"
     )
 
-    class Meta:
-        verbose_name = "car"
-        verbose_name_plural = "cars"
-
 
 class Driver(AbstractUser):
     license_number = models.CharField(max_length=100, unique=True)
-
-    class Meta:
-        verbose_name = "driver"
-        verbose_name_plural = "drivers"
