9a6e4ad2fca70cecdcadfaa9630e2c056fe5c16e
MaxymChyncha <max.chyncha@gmail.com>
2024-02-22 09:25:12 +0200

    Fixed imports

--- taxi/models.py
+++ taxi/models.py
@@ -1,7 +1,6 @@
+from django.conf import settings
 from django.contrib.auth.models import AbstractUser
 from django.db import models
-
-from taxi_service import settings
 
 
 class Driver(AbstractUser):
