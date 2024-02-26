6b30436dd2e3a4605474ac336149607585c7ddc3
PythonZem <pythonzem@gmail.com>
2024-02-26 09:47:01 +0200

    fix rel name

--- taxi/models.py
+++ taxi/models.py
@@ -20,5 +20,5 @@ class Driver(AbstractUser):
 
 class Car(models.Model):
     model = models.CharField(max_length=255)
-    manufacturer = models.ForeignKey(Manufacturer, on_delete=models.CASCADE, related_name="Manufacturers")
+    manufacturer = models.ForeignKey(Manufacturer, on_delete=models.CASCADE, related_name="manufacturers")
     drivers = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name="drivers")
