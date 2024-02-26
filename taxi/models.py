809bed65db9208dbe760ba079d5ac80bca02fa1e
yulias97 <yulkastetsko@gmail.com>
2024-02-26 11:55:04 +0200

    Fix solution

--- taxi/models.py
+++ taxi/models.py
@@ -1,7 +1,6 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
-
-from taxi_service import settings
+from django.conf import settings
 
 
 class Manufacturer(models.Model):
