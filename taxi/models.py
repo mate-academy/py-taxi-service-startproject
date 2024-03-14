fb448953a94dced4892e948c4ae1996423388a21
Oleksii Muhyr <1.2.aznch@gmai.com>
2024-03-14 19:48:58 +0200

    implement-py-taxi-service

---
+++ taxi/models.py
@@ -0,0 +1,31 @@
+from django.conf import settings
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class Manufacturer(models.Model):
+    name = models.CharField(max_length=256, unique=True)
+    country = models.CharField(max_length=256)
+
+    class Meta:
+        ordering = ["name",]
+
+    def __str__(self):
+        return f"{self.name}"
+
+
+class Car(models.Model):
+    model = models.CharField(max_length=256)
+    manufacturer = models.ForeignKey(
+        to=Manufacturer, related_name="cars", on_delete=models.CASCADE
+    )
+    drivers = models.ManyToManyField(
+        settings.AUTH_USER_MODEL, related_name="cars"
+    )
+
+    def __str__(self):
+        return f"{self.model}"
+
+
+class Driver(AbstractUser):
+    license_number = models.CharField(max_length=256, unique=True)
